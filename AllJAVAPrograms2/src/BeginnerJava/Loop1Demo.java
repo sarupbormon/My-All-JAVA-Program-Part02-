49b46111b5d97be60e2ecb3fdde7fa118ba85872
Sarup <sarupbormon18@gmail.com>
2024-10-08 00:57:09 +0530

    Program-29

---
+++ AllJAVAPrograms2/src/BeginnerJava/Loop1Demo.java
@@ -0,0 +1,19 @@
+package BeginnerJava;
+
+public class Loop1Demo {
+	
+	public static void main(String[] args) {
+
+		int i, sum = 0;
+		for( i = 1; i <= 10; i++) {
+			
+			sum = sum +i;
+			
+		}
+		System.out.println("The Sum is : "+sum);
+	}
+
+}
+
+	
+
